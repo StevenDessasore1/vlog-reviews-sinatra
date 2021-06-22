ee25169452bae46b65eba890b78771083ba79743
StevenDessasore <stevendessasoreba@gmail.com>
2021-06-22 07:04:39 -0400

    migrate reviews table

--- db/migrate/20210622104753_create_reviews.rb
+++ db/migrate/20210622104753_create_reviews.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class CreateReviews < ActiveRecord::Migration
+class CreateReviews < ActiveRecord::Migration[5.2]
   def change
     create_table :reviews do |t|
       t.string :content
