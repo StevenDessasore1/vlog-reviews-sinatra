4ae0f8e34d5219efe77bf1c81ba082674b2a3886
StevenDessasore <stevendessasoreba@gmail.com>
2021-06-22 03:59:09 -0400

    ./app/views/users/sign_in.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,37 +1,48 @@
 class UsersController < ApplicationController
 
-  # GET: /users
-  get "/users" do
-    erb :"/users/index.html"
-  end
-
   # GET: /users/new
-  get "/users/new" do
+
+  get "/create_account" do
     erb :"/users/new.html"
   end
 
-  # POST: /users
-  post "/users" do
-    redirect "/users"
+  get "/sign_in" do
+    erb :"/users/sign_in.html"
   end
+
+  post "/sign_out" do
+    #session.destroy
+   redirect '/'
+  end
+
+  # GET: /users
+  # get "/users" do
+  #  erb :"/users/index.html"
+  # end
+
+
+  # POST: /users
+  #post "/users" do
+  #  redirect "/users"
+  #end
 
   # GET: /users/5
-  get "/users/:id" do
-    erb :"/users/show.html"
-  end
+  #get "/users/:id" do
+   # erb :"/users/show.html"
+  #end
 
   # GET: /users/5/edit
-  get "/users/:id/edit" do
-    erb :"/users/edit.html"
-  end
+ # get "/users/:id/edit" do
+  #  erb :"/users/edit.html"
+ # end
 
   # PATCH: /users/5
-  patch "/users/:id" do
-    redirect "/users/:id"
-  end
+ # patch "/users/:id" do
+  #  redirect "/users/:id"
+ # end
 
   # DELETE: /users/5/delete
-  delete "/users/:id/delete" do
-    redirect "/users"
-  end
+ # delete "/users/:id/delete" do
+  #  redirect "/users"
+ # end
 end
