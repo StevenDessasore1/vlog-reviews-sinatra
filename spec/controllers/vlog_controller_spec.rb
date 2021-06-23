ef2347dcec88d1b16166a8bbafa88bd4a6a72d2a
StevenDessasore <stevendessasoreba@gmail.com>
2021-06-22 23:09:08 -0400

    remove code out of vlog_cont_spec file

--- spec/controllers/vlog_controller_spec.rb
+++ spec/controllers/vlog_controller_spec.rb
@@ -1,15 +1,6 @@
 # frozen_string_literal: true
 
-require_relative 'spec_helper'
+require_relative '../spec_helper'
 
-def app
-  ApplicationController
-end
-
-describe ApplicationController do
-  it 'responds with a welcome message' do
-    get '/'
-    expect(last_response.status).to eq(200)
-    expect(last_response.body).to include('Welcome to the Sinatra Template!')
-  end
+describe VlogsController do
 end
